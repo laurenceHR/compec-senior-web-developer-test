c3e6f7918e18fcdbecac1bcb34409589e0d7ca8b
Laurence <laurencehr@gmail.com>
2015-12-07 15:43:00 -0500

    Importacion a SQLite

--- import.php
+++ import.php
@@ -3,4 +3,25 @@
  * Crea las clases que creas necesarias para hacer el trabajo.
  * Ten en cuenta que el archivo `users.csv`
  */
+
+$_DWALR_US = 'DWALR/db/US/';
+$_DWALR_DA = 'DWALR/db/DA/pdo/';
+$_DWALR_DO = 'DWALR/db/DO/';
+
+require $_DWALR_US.'Usuario.US.class.php';
+
 $db = require __DIR__.'/db.php';
+
+$UsuarioUS = new UsuarioUS();
+$UsuarioUS->truncate();
+$UsuarioLista = $UsuarioUS->leerTxt('users.csv','|','-');
+foreach($UsuarioLista as $Usuario){
+   	//print_r($Usuario);echo '<br/>';
+   	
+   	if(! $UsuarioUS->agregar($Usuario) ){
+   		echo 'Error al Importar <br />';
+   		print_r($Usuario);
+   		echo '<br />';
+   	}
+
+}
